c5d112244208e456fd03b6569b9d17cd3ce27d2d
Scaffx <scaff.scaff444@gmail.com>
2026-04-13 18:32:12 -0300

    Atualização para mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.lovable.rpgbuddy',
   appName: 'rpg-buddy',
-  webDir: 'dist',
-  server: {
-    url: 'https://6f14b44e-edde-4153-9aa4-1093c4284f49.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
